498d1f0234ce40ced2edeb359c208005830af74d
murtadhoaaishah <aishatmurtadho@gmail.com>
2024-03-11 23:02:07 +0100

    add about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,64 +3,18 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <p>ABOUT US</p>
+      <p>WHO I AM</p>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, animi
-        corrupti. Aliquid cupiditate, facilis officia velit porro laborum ullam
-        qui esse, reprehenderit alias earum consequuntur incidunt totam
-        distinctio odio debitis neque architecto. Explicabo dignissimos beatae
-        quisquam, sunt quis qui quidem optio nobis placeat incidunt! Molestias
-        vel ullam impedit assumenda enim, voluptate ut corporis dolorum dicta
-        voluptatibus necessitatibus eligendi a ducimus, magni quaerat suscipit
-        nulla ex laboriosam exercitationem vitae placeat inventore neque
-        pariatur. Ea, sit officia nesciunt est quidem quae consequatur quam ad
-        veritatis ipsam deleniti temporibus blanditiis adipisci. Sed ipsum
-        praesentium quaerat nihil, vel recusandae accusamus eos voluptas fugiat
-        velit! Sunt neque repudiandae, atque eos, reiciendis accusantium ullam
-        architecto, doloremque repellat corporis adipisci itaque. Provident
-        veniam reiciendis voluptatibus quam! Iusto recusandae inventore
-        repudiandae quasi in ab voluptatem, laudantium, nostrum aperiam possimus
-        expedita nisi dolorem repellendus veniam quo eligendi minima rerum
-        soluta aliquam dolorum libero. Commodi eos fugiat ut hic laboriosam
-        magni iste omnis expedita quos molestias, saepe libero dolores aperiam
-        in temporibus? Ducimus facere consequatur odio magnam quia
-        necessitatibus magni itaque voluptates hic fugiat, reiciendis, unde
-        placeat quis porro blanditiis natus earum! Aliquid doloremque pariatur
-        veniam repudiandae recusandae amet ut itaque facilis cupiditate beatae
-        quis dolores, corrupti sit quisquam sequi sint esse quae vitae
-        consequuntur. Excepturi quia, reprehenderit autem distinctio eius, cum
-        assumenda blanditiis eveniet ducimus vel doloribus mollitia voluptatum
-        fuga, quidem amet temporibus provident dicta? Sunt quis ullam nisi
-        commodi, ab labore rem deserunt maiores id quae, harum tempore doloribus
-        dolorum, saepe aspernatur eum laboriosam quo deleniti architecto facere
-        delectus vel ipsa voluptate. Libero nisi non autem illo tenetur vero
-        expedita illum totam, doloribus ad! Optio iusto molestias quia molestiae
-        et odit! Obcaecati illum sint facere quia assumenda similique
-        consequuntur voluptatum non ut dolores eveniet, accusamus odio
-        perspiciatis doloremque maxime. Hic voluptate maxime, harum architecto
-        totam vel animi ut repudiandae ipsum vitae aliquam minus molestias
-        itaque quis velit quas labore quo non! Nulla dolorem nam earum provident
-        quos unde totam esse porro cum laboriosam officia consectetur obcaecati
-        aperiam blanditiis explicabo sed illo tempore eveniet sit dignissimos
-        dolore dolores, architecto facere! Adipisci inventore laudantium beatae
-        quod maxime nobis ipsa quasi hic perspiciatis sed quisquam incidunt odit
-        eius, eligendi, suscipit aliquam iste. Corporis aperiam enim, ut
-        necessitatibus accusantium sed facilis nostrum, nisi odit illum ea
-        reiciendis non voluptas excepturi! Fuga voluptates maiores, labore odit
-        nam obcaecati incidunt nemo nesciunt dolorem non. Asperiores possimus
-        quam distinctio. Omnis amet unde, blanditiis, laboriosam officiis, ipsa
-        quod libero quas expedita dolor vero delectus iure. Natus recusandae
-        accusantium neque corporis nisi dignissimos culpa, ullam similique
-        deserunt, saepe incidunt ex ad impedit, in enim repudiandae? Veniam
-        optio, reprehenderit totam ipsam dolorem consequuntur voluptatum
-        sapiente et, error debitis, cupiditate quae. Reiciendis eveniet, iure
-        cum ipsa laudantium dolore labore corrupti repellendus est. Nostrum
-        laboriosam iusto ipsam. Minus et molestias saepe, tempore asperiores
-        alias harum sapiente explicabo. Nam ducimus quas, voluptatibus
-        voluptatem molestias provident doloribus deleniti eligendi eos repellat
-        perferendis in tenetur sit id ullam rerum inventore rem quam tempore
-        aliquid distinctio at esse sunt? Sed nisi illum autem voluptates.
+        <span>Hi I am Murtadho 'Aaishah</span>A passionate frontend developer,
+        that brings creativity and precision to every project. With a strong
+        foundation in web technologies, I specialize in crafting engaging and
+        user-centric interfaces. My attention to detail, combined with a
+        collaborative mindset, allows me to deliver seamless and visually
+        appealing digital experiences. Eager to contribute my skills to
+        innovative projects and drive the future of web development.
       </p>
+      <professionCard />
     </div>
   );
 };
