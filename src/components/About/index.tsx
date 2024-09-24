17df2181012295e0468693d026f9795d13c133b5
murtadhoaaishah <murtadhoaishat@gmail.com>
2024-09-25 00:19:52 +0100

    aside and about restructured

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,32 +1,29 @@
 import React from "react";
+import "./About.scss";
 import { professions } from "../../data";
 import ProfessionCard from "../ProfessionCard";
 
 const About = () => {
   return (
-    <main className="h-screen pt-24 pl-10 pr-20">
+    <main className="about-container">
       <section className="mb-10">
-        <p className="text-[0.65rem] tracking-[0.5rem] mb-10">ABOUT US</p>
-        <p className="text-lg font-semibold name tracking-[0.5rem] mb-24">
-          WHO AM I
-        </p>
-        <p className="mb-8">
-          <span className="text-sm font-semibold mr-2">
-            Hi I am Murtadho 'Aaishah
-          </span>
-          A passionate frontend developer, that brings creativity and precision
-          to every project. With a strong foundation in web technologies, I
+        <p className="small-hdn">ABOUT US</p>
+        <p className="name big-hdn">WHO AM I</p>
+        <p className="mb-8 text">
+          <span className="about-name">Hi I am Murtadho 'Aaishah</span>A
+          passionate frontend developer, that brings creativity and precision to
+          every project. With a strong foundation in web technologies, I
           specialize in crafting engaging and user-centric interfaces. My
           attention to detail, combined with a collaborative mindset, allows me
           to deliver seamless and visually appealing digital experiences. Eager
           to
         </p>
-        <p>
+        <p className="text">
           contribute my skills to innovative projects and drive the future of
           webdevelopment.
         </p>
       </section>
-      <section className="flex flex-wrap space-x-8">
+      <section className="profession-card-wrp">
         {professions.map((profession, index) => (
           <ProfessionCard icon={profession.icon} title={profession.title} />
         ))}
@@ -34,5 +31,4 @@ const About = () => {
     </main>
   );
 };
-
 export default About;
