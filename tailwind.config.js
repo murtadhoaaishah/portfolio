069ccfe88e0e4b25e15fd0dfb1c99da125d3ff2b
murtadhoaaishah <murtadhoaishat@gmail.com>
2024-09-24 21:37:04 +0100

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ export default {
       colors: {
         blue: "#2d98f0",
         ash: "#f2f3f7",
-        gray: "",
+        gray: "#999999",
+        gold: "#f9bf3f",
+        red: "#ec5453",
+        purple: "#a84cb8",
+        ligbtBlack: "#000000b3",
+        green: "#2fa499",
+        darkBlue: "#4054b2",
       },
       backgroundImage: {
         hero: "url('/public/images/IMG_1502.PNG')",
