23c1ec35bee09051483a7da26e82aa6e8a4528eb
murtadhoaaishah <murtadhoaishat@gmail.com>
2024-10-11 23:51:34 +0100

    projrct count style updated

--- src/components/ProfessionCard/index.tsx
+++ src/components/ProfessionCard/index.tsx
@@ -3,10 +3,10 @@ import { professions } from "../../data";
 
 const ProfessionCard = ({ icon, title }: { icon: string; title: string }) => {
   return (
-    <div className="w-48 h-36 flex flex-col p-6 shadow-xl">
+    <div className="pro-card-cont">
       {" "}
-      <img src={icon} className="w-8 mb-6 border-gray" />
-      <p className="font-medium">{title}</p>
+      <img src={icon} className="" />
+      <p className="">{title}</p>
     </div>
   );
 };
--- src/components/ProjectCount/index.tsx
+++ src/components/ProjectCount/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const ProjectCount = () => {
   return (
-    <div className="ml-10 mr-20 p-8 bg-gold h-58 mb-52">
-      <p className="text-4xl  name mb-2text-gray">I am happy to tell you</p>
-      <p className="text-4xl  name mb-10 text-gray">
+    <div className="proj-count-cont">
+      <p className="happy">I am happy to tell you</p>
+      <p className="proj-count name">
         that 5<span>+</span> projects done successfully
       </p>
-      <span className="border py-1 px-4 gray">HIRE ME</span>
+      <span className="">HIRE ME</span>
     </div>
   );
 };
